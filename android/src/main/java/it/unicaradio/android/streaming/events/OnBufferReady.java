e39c54dc04621ed50ea157eaaa68c4735a4b7743
paolo.cortis <paolo.cortis@ef1f2e14-d98e-a71b-b804-03944a38e0bc>
2011-05-17 14:04:38 +0000

    [ANDROID] New classes
    git-svn-id: https://unicaradio-apps.googlecode.com/svn/trunk@17 ef1f2e14-d98e-a71b-b804-03944a38e0bc

---
+++ android/src/main/java/it/unicaradio/android/streaming/events/OnBufferReady.java
@@ -0,0 +1,35 @@
+/**
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Copyright UnicaRadio
+ */
+package it.unicaradio.android.streaming.events;
+
+import java.util.EventObject;
+
+/**
+ * @author Paolo Cortis
+ * 
+ */
+public class OnBufferReady extends EventObject
+{
+
+	private static final long serialVersionUID = -1170398129805681921L;
+
+	public OnBufferReady(Object source)
+	{
+		super(source);
+	}
+
+}
