159e821a6f4733ad72bdf3ae99774011a71c7848
paolo.cortis@gmail.com <paolo.cortis@gmail.com@ef1f2e14-d98e-a71b-b804-03944a38e0bc>
2011-08-13 08:56:12 +0000

    #93 Inserire un Service per la gestione dell'audio
    git-svn-id: https://unicaradio-apps.googlecode.com/svn/trunk@63 ef1f2e14-d98e-a71b-b804-03944a38e0bc

---
+++ android/src/it/unicaradio/android/services/StreamingService.java
@@ -0,0 +1,242 @@
+/**
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Copyright UnicaRadio
+ */
+package it.unicaradio.android.services;
+
+import it.unicaradio.android.events.OnInfoListener;
+import it.unicaradio.android.gui.TrackInfos;
+import it.unicaradio.android.streamers.IcecastStreamer;
+import it.unicaradio.android.streamers.Streamer;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.MessageFormat;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.spoledge.aacplayer.AACPlayer;
+import com.spoledge.aacplayer.ArrayAACPlayer;
+import com.spoledge.aacplayer.ArrayDecoder;
+import com.spoledge.aacplayer.Decoder;
+
+/**
+ * @author Paolo Cortis
+ */
+public class StreamingService extends Service
+{
+	public static final String ACTION_TRACK_INFO = "it.unicaradio.android.intent.action.TRACK_INFO";
+
+	private static final String LOG = StreamingService.class.getName();
+
+	private static final String STREAM_URL = "http://streaming.unicaradio.it:80/unica64.aac";
+
+	private final IBinder mBinder = new LocalBinder();
+
+	private URLConnection conn;
+
+	private Streamer streamer;
+
+	private AACPlayer player;
+
+	private final TrackInfos infos = new TrackInfos();
+
+	private boolean isPlaying;
+
+	private String error = "";
+
+	private final BroadcastReceiver connectivityBroadcastReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent)
+		{
+			boolean noConnectivity = intent.getBooleanExtra(
+					ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+
+			if(noConnectivity) {
+				stop();
+			}
+		}
+	};
+
+	private final BroadcastReceiver telephonyBroadcastReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent)
+		{
+			PhoneStateListener phoneListener = new PhoneStateListener() {
+				@Override
+				public void onCallStateChanged(int state, String incomingNumber)
+				{
+					switch(state) {
+						case TelephonyManager.CALL_STATE_OFFHOOK:
+							stop();
+							break;
+						case TelephonyManager.CALL_STATE_RINGING:
+							stop();
+							break;
+					}
+				}
+			};
+			TelephonyManager telephony = (TelephonyManager) context
+					.getSystemService(Context.TELEPHONY_SERVICE);
+			telephony.listen(phoneListener,
+					PhoneStateListener.LISTEN_CALL_STATE);
+		}
+	};
+
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		return mBinder;
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		return START_NOT_STICKY;
+	}
+
+	@Override
+	public void onDestroy()
+	{
+		stop();
+		super.onDestroy();
+	}
+
+	public boolean isPlaying()
+	{
+		return isPlaying;
+	}
+
+	public void play()
+	{
+		if(conn == null) {
+			registerReceiver(connectivityBroadcastReceiver, new IntentFilter(
+					ConnectivityManager.CONNECTIVITY_ACTION));
+			registerReceiver(telephonyBroadcastReceiver, new IntentFilter(
+					TelephonyManager.ACTION_PHONE_STATE_CHANGED));
+			try {
+				URL url = new URL(STREAM_URL);
+				_play(url);
+			} catch(MalformedURLException e) {
+				stopWithException(
+						"Errore: l'indirizzo di streaming non è corretto.", e);
+			} catch(IOException e) {
+				stopWithException(e);
+			}
+		} else {
+			stop();
+		}
+	}
+
+	public void stop()
+	{
+		try {
+			unregisterReceiver(connectivityBroadcastReceiver);
+		} catch(IllegalArgumentException e) {
+		}
+		try {
+			unregisterReceiver(telephonyBroadcastReceiver);
+		} catch(IllegalArgumentException e) {
+		}
+
+		if(player != null) {
+			player.stop();
+			player = null;
+			streamer = null;
+			conn = null;
+			isPlaying = false;
+			infos.clean();
+			notifyChange();
+		}
+	}
+
+	private void _play(URL url) throws IOException
+	{
+		if(conn == null) {
+			conn = url.openConnection();
+			conn.addRequestProperty("Icy-MetaData", "1");
+			conn.connect();
+			streamer = new IcecastStreamer(conn);
+
+			streamer.addOnInfoListener(new OnInfoListener() {
+				public void onInfo(TrackInfos trackInfos)
+				{
+					infos.setTrackInfos(trackInfos);
+					notifyChange();
+				}
+			});
+
+			player = new ArrayAACPlayer(
+					ArrayDecoder.create(Decoder.DECODER_OPENCORE));
+
+			Thread playThread = new Thread(new Runnable() {
+				public void run()
+				{
+					try {
+						isPlaying = true;
+						player.play(streamer);
+					} catch(Exception e) {
+						stopWithException(e);
+					}
+				}
+			});
+			playThread.start();
+		}
+	}
+
+	private void stopWithException(String message, Exception e)
+	{
+		error = MessageFormat.format("{0}: {1}", message, e.getMessage());
+		stop();
+		Log.d(LOG, message, e);
+	}
+
+	private void stopWithException(Exception e)
+	{
+		stopWithException("E' avvenuto un problema. Riprova.", e);
+	}
+
+	public void notifyChange()
+	{
+		Intent i = new Intent(ACTION_TRACK_INFO);
+		i.putExtra("author", infos.getAuthor());
+		i.putExtra("title", infos.getTitle());
+		i.putExtra("isplaying", isPlaying);
+		i.putExtra("error", error);
+
+		sendBroadcast(i);
+		error = "";
+	}
+
+	public class LocalBinder extends Binder
+	{
+		public StreamingService getService()
+		{
+			return StreamingService.this;
+		}
+	}
+}
