76314072b6e3e55ded43c17305fc8e7540a36ac8
paolo.cortis@gmail.com <paolo.cortis@gmail.com@ef1f2e14-d98e-a71b-b804-03944a38e0bc>
2011-09-03 10:32:18 +0000

    #106 Il menu DEVE essere presente in tutte le activity
    git-svn-id: https://unicaradio-apps.googlecode.com/svn/trunk@67 ef1f2e14-d98e-a71b-b804-03944a38e0bc

--- android/src/it/unicaradio/android/activities/TabbedActivity.java
+++ android/src/it/unicaradio/android/activities/TabbedActivity.java
@@ -33,6 +33,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -74,6 +77,29 @@ public abstract class TabbedActivity extends Activity
 		}
 
 		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle item selection
+		switch(item.getItemId()) {
+			case R.id.exit:
+				System.runFinalizersOnExit(true);
+				System.exit(0);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 
 	public void openLink(String url)
