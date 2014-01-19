fff8e1703642e6ef18635efa1dd31177264481f5
Paolo Cortis <paolo.cortis@gmail.com>
2014-01-19 21:04:12 +0100

    [iOS] UI-36 [BUG] Schedule: when format name is long the time is not visible

---
+++ iOS/Streaming/widgets/ScheduleTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ScheduleTableViewCell.h
+//  Streaming
+//
+//  Created by Paolo Cortis on 19/01/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScheduleTableViewCell : UITableViewCell
+
+@end
---
+++ iOS/Streaming/widgets/ScheduleTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  ScheduleTableViewCell.m
+//  Streaming
+//
+//  Created by Paolo Cortis on 19/01/14.
+//
+//
+
+#import "ScheduleTableViewCell.h"
+
+@implementation ScheduleTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
